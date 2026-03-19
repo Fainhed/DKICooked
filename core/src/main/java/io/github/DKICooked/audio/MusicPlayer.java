ffd09006b20a0c25ac8e4eb9dea0e9d6720da8de
fainhed <timothylontac@gmail.com>
2026-03-19 13:48:27 +0800

    Added a difference instance background music provider

---
+++ core/src/main/java/io/github/DKICooked/audio/MusicPlayer.java
@@ -0,0 +1,35 @@
+package io.github.DKICooked.audio;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
+public class MusicPlayer {
+
+    private Clip clip;
+
+    public void play(String path, boolean loop) {
+        stop(); // stop current music first
+
+        try {
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(new File(path));
+            clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+
+            if (loop) {
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
+            }
+        // in case error
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stop() {
+        if (clip != null && clip.isRunning()) {
+            clip.stop();
+            clip.close();
+        }
+    }
+}
